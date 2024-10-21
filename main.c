4739d97cc8e1bd579298e2b5dffd7a35e675860a
Ketan Sonar <ketan.sonar.798@gmail.com>
2024-10-22 03:08:00 +0530

    add collision detection

--- main.c
+++ main.c
@@ -19,20 +19,20 @@ typedef struct Player {
     Vector2 vel;
 } Player;
 
-Rectangle obstacles[MAX_OBSTACLES] = { 0 };
+typedef struct Obstacle {
+    Vector2 pos;
+    Vector2 vel;
+    size_t frame_counter;
+    size_t frame_speed;
+    size_t current_frame;
+} Obstacle;
+
+Obstacle obstacles[MAX_OBSTACLES] = { 0 };
+
+bool game_over = false;
 
 int main(void)
 {
-    size_t spacing = 0;
-    for (size_t i = 0; i < MAX_OBSTACLES; ++i) {
-        obstacles[i].width = (float)GetRandomValue(50, 100);
-        obstacles[i].height = (float)GetRandomValue(50, 100);
-        obstacles[i].x = spacing;
-        obstacles[i].y = GROUND_Y - obstacles[i].height;
-
-        spacing += (size_t)obstacles[i].width + GetRandomValue(200, 400);
-    }
-
     InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Geometry Dash");
 
     Texture2D scarfy = LoadTexture("./assets/scarfy.png");
@@ -46,6 +46,17 @@ int main(void)
     float scrollingMid = 0.0f;
     float scrollingFore = 0.0f;
 
+    Texture2D explosions[7];
+    for (size_t i = 0; i < 7; ++i) {
+        char buf[35];
+        snprintf(buf, 35, "./assets/explosion/explosion_%c.png", '1'+(int)i);
+        explosions[i] = LoadTexture(buf);
+    }
+
+    size_t explosion_frame_counter = 0;
+    size_t explosion_current_frame = 0;
+    size_t explosion_frame_speed = 8;
+
     Texture2D fireballs[5];
     for (size_t i = 0; i < 5; ++i) {
         char buf[33];
@@ -53,10 +64,23 @@ int main(void)
         fireballs[i] = LoadTexture(buf);
     }
 
+    size_t spacing = 0;
+    for (size_t i = 0; i < MAX_OBSTACLES; ++i) {
+        obstacles[i].pos.x = spacing;
+        obstacles[i].pos.y = GROUND_Y - fireballs[0].height * (GetRandomValue(0, 10) > 5 ? 0.5f : (GetRandomValue(0, 10) > 5 ? 0.5f : 1.5f));
+        spacing += (size_t)fireballs[0].width + GetRandomValue(600, 800);
+
+        obstacles[i].vel = (Vector2){ .x = 0, .y = 0 };
+
+        obstacles[i].frame_counter = 0;
+        obstacles[i].current_frame = 0;
+        obstacles[i].frame_speed = 8;
+    }
+
     Player player = {
         .size = { .x = scarfy.width, .y = scarfy.height },
         .pos  = { .x = WINDOW_WIDTH/2.0f, .y = WINDOW_HEIGHT/2.0f },
-        .vel  = { .x = 300, .y = 0 },
+        .vel  = { .x = 400, .y = 0 },
     };
 
     Camera2D camera = { 0 };
@@ -71,11 +95,51 @@ int main(void)
 
     SetTargetFPS(60);
     while (!WindowShouldClose()) {
+        if (game_over) {
+            ++explosion_frame_counter;
+            if (explosion_frame_counter >= (60/explosion_frame_speed)) {
+                explosion_frame_counter = 0;
+                if (explosion_current_frame > 5) {
+                    goto drawing;
+                }
+            }
+            goto drawing;
+        }
+
         ++sprite_frame_counter;
         if (sprite_frame_counter >= (60/sprite_frame_speed)) {
             sprite_frame_counter = 0;
             if (++sprite_current_frame > 5) sprite_current_frame = 0;
             frame_rec.x = sprite_current_frame*scarfy.width/6.0f;
+        }
+
+        for (size_t i = 0; i < MAX_OBSTACLES; ++i) {
+            obstacles[i].frame_counter++;
+            if (obstacles[i].frame_counter >= (60/obstacles[i].frame_speed)) {
+                obstacles[i].frame_counter = 0;
+                obstacles[i].current_frame++;
+                if (obstacles[i].current_frame > 4) obstacles[i].current_frame = 0;
+            }
+
+            obstacles[i].pos = Vector2Add(obstacles[i].pos, obstacles[i].vel);
+
+            float playerLeft = player.pos.x - frame_rec.width/2.0f;
+            float playerRight = playerLeft + frame_rec.width;
+            float playerTop = player.pos.y;
+            float playerBottom = playerTop + frame_rec.height;
+
+            float obstacleLeft = obstacles[i].pos.x;
+            float obstacleRight = obstacleLeft + fireballs[0].width * 0.3f;
+            float obstacleTop = obstacles[i].pos.y;
+            float obstacleBottom = obstacleTop + fireballs[0].height * 0.3f;
+
+            if (playerRight > obstacleLeft &&
+                playerLeft < obstacleRight &&
+                playerBottom > obstacleTop &&
+                playerTop < obstacleBottom) {
+                game_over = true;
+                break;
+            }
         }
 
         scrollingBack -= 0.1f*4;
@@ -104,6 +168,7 @@ int main(void)
             player.vel.y = 0;
         }
 
+drawing:
         BeginDrawing();
 
             ClearBackground(ColorTint(GetColor(0x052c46ff), GRAY));
@@ -132,13 +197,16 @@ int main(void)
                 DrawTextureRec(scarfy, frame_rec,
                                (Vector2){ .x = player.pos.x - frame_rec.width/2.0f, .y = player.pos.y },
                                WHITE);
-                // DrawLineEx(
-                //     (Vector2){ .x = -WINDOW_WIDTH/2.0f, .y = GROUND_Y },
-                //     (Vector2){ .x = obstacles[MAX_OBSTACLES-1].x, .y = GROUND_Y },
-                //     2.0f,
-                //     DARKGRAY
-                // );
 
+                for (size_t i = 0; i < MAX_OBSTACLES; ++i) {
+                    DrawTextureEx(fireballs[obstacles[i].current_frame], obstacles[i].pos, 0, 0.3f, WHITE);
+                }
+
+                if (game_over) {
+                    DrawTextureEx(explosions[explosion_current_frame],
+                                  (Vector2){ .x = player.pos.x - explosions[explosion_current_frame].width*0.8f/2.0f, .y = GROUND_Y - explosions[explosion_current_frame].height*0.8f }, 0, 0.8f, WHITE);
+                }
+            
             EndMode2D();
 
         EndDrawing();
